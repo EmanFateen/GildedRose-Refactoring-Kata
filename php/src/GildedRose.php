9f9d71088f8cd179d0d25be2089b14137ea6b8dd
EmanFateen <emanfateen@gmail.com>
2025-11-23 13:14:57 +0200

    chore: refactor  GildedRose

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -18,19 +18,12 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name === 'Sulfuras, Hand of Ragnaros') {
-                $item->quality = $item->qualityCalculator->calculateQuality($item->sellIn, $item->quality);
-                continue;
-            }
-            if ($item->name === 'Aged Brie') {
-                $item->quality = $item->qualityCalculator->calculateQuality($item->sellIn, $item->quality);
-            } else if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
-                $item->quality = $item->qualityCalculator->calculateQuality($item->sellIn, $item->quality);
-            } else {
-                $item->quality = $item->qualityCalculator->calculateQuality($item->sellIn, $item->quality);
-            }
 
-            $item->sellIn--;
+            $item->quality = $item->qualityCalculator->calculateQuality($item->sellIn, $item->quality);
+
+            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
+                $item->sellIn--;
+            }
         }
     }
 
