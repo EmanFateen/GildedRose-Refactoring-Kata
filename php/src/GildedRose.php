43abadbf1cd7c465f8a7f159efa3455a1eeb9940
EmanFateen <emanfateen@gmail.com>
2025-11-24 22:56:59 +0200

    chore: correct logic

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -23,29 +23,41 @@ final class GildedRose
 
             if ($item->name === 'Aged Brie') {
                 $item->quality = min(50, ++$item->quality);
+
                 $item->sellIn--;
+
+                if ($item->sellIn < 0) {
+                    $item->quality = min(50, ++$item->quality);
+                }
+
                 continue;
             }
 
             if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->sellIn <= 1) {
-                    $item->quality = 0;
-                } else if ($item->sellIn <= 5) {
-                    $item->quality = min(50, $item->quality + 3);
-                } else if ($item->sellIn <= 10) {
+                $item->quality = min(50, ++$item->quality);
+
+                if ($item->sellIn <= 5) {
                     $item->quality = min(50, $item->quality + 2);
+                } else if ($item->sellIn <= 10) {
+                    $item->quality = min(50, $item->quality + 1);
                 }
 
                 $item->sellIn--;
+
+                if ($item->sellIn < 0) {
+                    $item->quality = 0;
+                }
                 continue;
 
             }
 
             $item->quality = max(0, $item->quality - 1);
-            if ($item->sellIn <= 1) {
+
+            $item->sellIn--;
+
+            if ($item->sellIn < 0) {
                 $item->quality = max(0, $item->quality - 1);
             }
-            $item->sellIn--;
         }
     }
 
